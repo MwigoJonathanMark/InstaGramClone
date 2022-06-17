adfbdf68acbe9c21b25fd3f41b2c29cb3dcd897d
JonMarkM <mwigojm@gmail.com>
2022-06-17 09:50:01 +0300

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-3">
+            <img class="rounded-circle" src="/images/download (8).jpg" alt="" width="120px" height="100px">
+        </div>
+        <div class="col-md-8">
+        </div>
+    </div>
+</div>
+@endsection
